5a827ab3366e00335272a01fbdb89703c2ae259a
rosencrantz96 <topgdvidsyb@naver.com>
2023-01-12 14:58:59 +0900

    수업

---
+++ src/main/java/com/myshop/dto/ItemDto.java
@@ -0,0 +1,26 @@
+package com.myshop.dto;
+
+import java.time.LocalDateTime;
+import com.myshop.constant.ItemSellStatus;
+
+import lombok.*;
+
+@Getter
+@Setter 
+public class ItemDto {
+	private Long id; // 상품코드 
+	 
+	private String itemNm; // 상품명
+	
+	private int price; // 가격
+	
+	private int stockNumber; // 재고수량
+	
+	private String itemDetail; // 상품 상세 설명 
+	
+	private ItemSellStatus itemSellStatus; // 상품 판매 상태 
+	
+	private LocalDateTime regTime; // 등록 시간
+
+	private LocalDateTime updateTime; // 수정 시간
+}
