1585d88bc968b9ad069e8b40a9e873c124e8434b
rosencrantz96 <topgdvidsyb@naver.com>
2023-01-28 17:02:58 +0900

    장바구니 수정해야 함

--- src/main/java/com/myshop/dto/CartOrderDto.java
+++ src/main/java/com/myshop/dto/CartOrderDto.java
@@ -1,5 +1,15 @@
 package com.myshop.dto;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter 
 public class CartOrderDto {
 
+    private Long cartItemId;
+
+    private List<CartOrderDto> cartOrderDtoList;
 }
--- src/main/java/com/myshop/repository/CartItemRepository.java
+++ src/main/java/com/myshop/repository/CartItemRepository.java
@@ -1,5 +1,20 @@
 package com.myshop.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.myshop.dto.CartDetailDto;
+import com.myshop.entity.CartItem;
+
 public interface CartItemRepository {
+
+	CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+	@Query("select new com.haseoshop.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) "
+			+ "from CartItem ci, ItemImg im " + "join ci.item i " + "where ci.cart.id = :cartId "
+			+ "and im.item.id = ci.item.id " + "and im.repimgYn = 'Y' " + "order by ci.regTime desc")
+	List<CartDetailDto> findCartDetailDtoList(@Param("cartId") Long cartId);
 
 }
